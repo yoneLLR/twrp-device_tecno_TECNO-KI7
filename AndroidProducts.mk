e8befeb20701b647f630af128231508919fd32f8
justyone! <orcaleskeiano@gmail.com>
2024-10-30 22:13:02 +0800

    Tecno-KI7: Initial Skeleton TWRP tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_KI7.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_KI7-user \
+    twrp_KI7-userdebug \
+    twrp_KI7-eng
